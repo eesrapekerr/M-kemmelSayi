6e1bdcf7b2ffdf5a546de1d3d857ee294ec3ec62
eesrapekerr <33705100+eesrapekerr@users.noreply.github.com>
2019-11-22 09:43:34 +0300

    Update Program.cs

--- MükemmelSayi/Program.cs
+++ MükemmelSayi/Program.cs
@@ -6,7 +6,45 @@ namespace MükemmelSayi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Bir Sayı Giriniz");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+            //int toplam = 0;
+            //for(int i=1; i<sayi; i++)
+            //{
+            //    if (sayi%i==0)
+            //    {
+            //        toplam += i;
+            //        Console.WriteLine(sayi + "Sayı Mükemmeldir");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(sayi + "Sayı Mükemmel değildir!");
+            //    }
+            //    Console.ReadLine();
+            //}
+
+            for (int i=1; i<1000; i++)
+            {
+                int toplam = 0;
+                for (int k =1;k<i; k++ )
+                {
+                    if ( i%k==0)
+                    {
+                        toplam += k; 
+                        
+                    }
+
+                    else
+                    {
+                        Console.WriteLine(k + " Sayı Mükemmel değildir.");
+                    }
+                }
+                if (i == toplam)
+                {
+                    Console.WriteLine(i + " Sayı Mükemmeldir");
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
