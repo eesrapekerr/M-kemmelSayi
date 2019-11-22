a86574b864eae23efba8a8bcc39892de1e3d3a7e
eesrapekerr <33705100+eesrapekerr@users.noreply.github.com>
2019-11-22 09:04:50 +0300

    Add project files.

---
+++ MükemmelSayi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MükemmelSayi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
